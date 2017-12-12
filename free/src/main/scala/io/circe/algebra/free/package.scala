7793cb43c53ad32c0c35755557ff5818bd638aa8
Travis Brown <travisbrown@stripe.com>
2017-12-12 15:08:07 +0000

    Use DecodingFailure in free implementation

--- free/src/main/scala/io/circe/algebra/free/package.scala
+++ free/src/main/scala/io/circe/algebra/free/package.scala
@@ -2,28 +2,28 @@ package io.circe.algebra
 
 import cats.{ ~>, Monad, Traverse }
 import cats.data.{ IndexedStateT, StateT }
-import io.circe.{ DecodingFailure, Error, Json }
+import io.circe.{ DecodingFailure, Json }
 
 package object free {
-  type ErrorOr[A] = Either[Error, A]
+  type ErrorOr[A] = Either[DecodingFailure, A]
   type ErrorOrState[A] = StateT[ErrorOr, Json, A]
 
   private[this] implicit val errorOrMonad: Monad[ErrorOr] =
-    cats.instances.either.catsStdInstancesForEither[Error]
+    cats.instances.either.catsStdInstancesForEither[DecodingFailure]
 
   private[this] implicit val errorOrStateMonad: Monad[ErrorOrState] =
     IndexedStateT.catsDataMonadForIndexedStateT[ErrorOr, Json](errorOrMonad)
 
-  private[this] def traverseVector[A, B](as: Vector[A])(f: A => ErrorOr[B]): Either[Error, Vector[B]] =
+  private[this] def traverseVector[A, B](as: Vector[A])(f: A => ErrorOr[B]): Either[DecodingFailure, Vector[B]] =
     (cats.instances.vector.catsStdInstancesForVector: Traverse[Vector]).traverse[ErrorOr, A, B](as)(f)
 
-  def decode[A](j: Json)(implicit decodeA: Decoder[A]): Either[Error, A] =
+  def decode[A](j: Json)(implicit decodeA: Decoder[A]): Either[DecodingFailure, A] =
     decodeA.op.foldMap[ErrorOrState](eitherInterpreter).runA(j)
 
   val eitherInterpreter: Op ~> ErrorOrState = new (Op ~> ErrorOrState) {
     def inspect[A](message: String)(f: Json => Option[A]): ErrorOrState[A] =
       StateT.inspectF[ErrorOr, Json, A](
-        f(_).fold[Either[Error, A]](
+        f(_).fold[Either[DecodingFailure, A]](
           Left(DecodingFailure(message, Nil))
         )(Right(_))
       )
